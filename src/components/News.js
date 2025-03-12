fedebbab1297524f2e7b11d57b09969c0e41e99d
tarunkuma707 <76809697+tarunkuma707@users.noreply.github.com>
2025-03-12 22:32:13 +0530

    Loopiing througth an array on newsapi Video 26 React Js Code Harry

---
+++ src/components/News.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+export class News extends Component {
+    articles =
+    [
+      {
+        "source": {
+          "id": "news24",
+          "name": "News24"
+        },
+        "author": "Lloyd Burnard",
+        "title": "Cricket quotas: Warriors, fined R500 000, launch formal dispute after transformation breach",
+        "description": "As transformation policies at domestic level continue to be questioned, the Warriors have launched a formal dispute against Cricket South Africa (CSA) after they were disqualified from the 1-Day Cup on Monday.",
+        "url": "https://www.news24.com/sport/cricket/quotas-in-cricket-warriors-fined-r500-000-launch-formal-dispute-after-transformation-breach-20250311",
+        "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/12785/643a5f1d19c04c22b72afe8c7e877fa7.jpg",
+        "publishedAt": "2025-03-11T13:53:44",
+        "content": "As transformation policies at domestic level continue to be questioned, the Warriors have launched a formal dispute against Cricket South Africa (CSA) after they were disqualified from the 1-Day Cup … [+9 chars]"
+      },
+      {
+        "source": {
+          "id": "espn-cric-info",
+          "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+        "publishedAt": "2020-04-27T11:41:47Z",
+        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+      },
+      {
+        "source": {
+          "id": "espn-cric-info",
+          "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        "publishedAt": "2020-03-30T15:26:05Z",
+        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+      }
+    ]
+    constructor(){
+        super();
+        this.state = {            
+            articles: this.articles,
+            loading:false   
+        }
+    }
+  render() {
+    return (
+      <div className='container my-3'>
+        <h2>News Monkey - Top Headlines</h2>
+        
+        <div className="row">
+            {this.state.articles.map((element)=>{
+                return <div className="col-md-4" key={element.url}>
+                    <Newsitem title={element.title.slice(0,40)} description={element.description.slice(0,80)} imageUrl={element.urlToImage} newsUrl={element.url} />
+                </div>
+            })}
+            
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
