9cfc89e4845124b9056ad14847734d458abcbbf3
tarunkuma707 <76809697+tarunkuma707@users.noreply.github.com>
2025-03-13 11:16:48 +0530

    Using Fetch API in react from NewsApi to populate News Item ReactJS Video No 27 Code With Harry

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
   render() {
+    let titleSytle ={
+      textOverflow  :   "ellipsis",
+      overflow      :   "hidden",
+      whiteSpace    :   "nowrap"
+    }
+    let imageStyle  = {
+      height    : "40vh"
+    }
+    let descStyle = {
+      textOverflow  :   "ellipsis",
+      overflow      :   "hidden",
+      whiteSpace    :   "nowrap",
+      height        :   "12vh"
+    }
     let {title, description,imageUrl,newsUrl}  =  this.props;
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img style={imageStyle} src={imageUrl===null ? "https://www.indiantelevision.com/sites/default/files/styles/smartcrop_800x800/public/images/tv-images/2021/08/02/news.jpg" : imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{ title }</h5>
-                <p className="card-text">{ description }</p>
+                <h5 className="card-title" style={titleSytle}>{ title }</h5>
+                <p className="card-text" style={descStyle}>{ description }</p>
                 <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
